f92cc54770342f9496b90fdd8e5c15de09f2c426
Kevin Rangel Moreira <rangel.moreira@outlook.com>
2023-02-21 11:31:07 -0300

    Theme color change project developed with angular

---
+++ src/app/core/components/header/enums/ETheme.enum.ts
@@ -0,0 +1,6 @@
+export enum Etheme {
+  ICON_MOON = 'moon',
+  ICON_SUN = 'sun',
+  TEXT_SUN = 'Dark Theme',
+  TEXT_MOON = 'Light Theme',
+}
---
+++ src/app/core/components/header/header.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+
+//Enum
+import { Etheme } from './enums/ETheme.enum';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent implements OnInit {
+
+  public icon: string =  Etheme.ICON_MOON;
+  public textTheme: string = Etheme.TEXT_MOON;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  public toggle(){
+    const theme = document.body.classList.toggle('dark-theme');
+
+    if (theme){
+      this.textTheme =  Etheme.TEXT_SUN;
+      return this.icon = Etheme.ICON_SUN;
+    }
+    this.textTheme =  Etheme.TEXT_MOON;
+    return this.icon = Etheme.ICON_MOON;
+  }
+}
